c67bca063cc3fdee0c6b10139aed51d0ec9b9768
Akash <akash300383@gmail.com>
2023-07-17 16:48:19 +0530

    done

--- workspace/ProductManagementSystem/src/pms/main/App.java
+++ workspace/ProductManagementSystem/src/pms/main/App.java
@@ -2,6 +2,7 @@ package pms.main;
 
 import java.util.Scanner;
 
+import pms.bean.Product;
 import pms.service.ProductService;
 
 public class App {
@@ -15,19 +16,29 @@ public class App {
 	String con;
 	int choice;
 	String result;
-	
 	do {
+		
 		System.out.println("1:Add Product 2:Number Of Product");
 		System.out.println("Plz enter your choice");
 		choice = sc.nextInt();
 		switch(choice) {
 		case 1:System.out.println("Add Product");
+		       System.out.println("Enter the product id");
+		       pid = sc.nextInt();
+		       System.out.println("Enter the product name");
+		       pname = sc.next();
+		       System.out.println("Enter the product price");
+		       price = sc.nextFloat();
+		       Product p = new Product(pid, pname, price); // store in product object. 
+		       result = ps.storeProduct(p);
+		       System.out.println(result);
 		       break;
 		case 2:System.out.println("Number of product");
 		       break;
 		 default:System.out.println("Wrong choice");
 		       break;
 		}
+		
 		System.out.println("do you want to continue?");
 		con = sc.next();
 	} while (con.equalsIgnoreCase("y"));
