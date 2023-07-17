35dd035ff5c73bf2dfa1e1784244703f92d393d8
Akash <akash300383@gmail.com>
2023-07-17 16:30:11 +0530

    done

---
+++ workspace/ProductManagementSystem/src/pms/main/App.java
@@ -0,0 +1,20 @@
+package pms.main;
+
+import java.util.Scanner;
+
+import pms.service.ProductService;
+
+public class App {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	ProductService ps =new ProductService();
+	int pid;
+	String pname;
+	float price;
+	String con;
+	int choice;
+	String result;
+	}
+
+}
