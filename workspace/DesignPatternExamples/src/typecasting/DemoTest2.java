ea38a57c3588d4b08a224540291e5d57362e39b9
Akash <akash300383@gmail.com>
2023-07-19 14:48:58 +0530

    done

--- workspace/DesignPatternExamples/src/typecasting/DemoTest2.java
+++ workspace/DesignPatternExamples/src/typecasting/DemoTest2.java
@@ -14,18 +14,25 @@ class B extends A {
 }
 public class DemoTest2 {
 	public static void main(String[] args) {
-		A obj1 = new A();		// creating super class object. 
-	obj1.dis1();
-		B obj2 = new B();		// creating sub class object 
-	obj2.dis1();       // method overriding 
-	obj2.dis2();
-	//B obj3 =new A();	// creating super class object and sub class reference not possible.
-		A obj4 = new B();	// sub class object and super class reference possible : implicit type casting. 
-		obj4.dis1(); // with help of obj4 we can call only those method which belong to super class
-		//obj4.dis2();
-		B obj5 = (B) obj4;  // down level type casting 
-		obj5.dis1();
-		obj5.dis2();
+//		A obj1 = new A();		// creating super class object. 
+//	obj1.dis1();
+//		B obj2 = new B();		// creating sub class object 
+//	obj2.dis1();       // method overriding 
+//	obj2.dis2();
+//	//B obj3 =new A();	// creating super class object and sub class reference not possible.
+//		A obj4 = new B();	// sub class object and super class reference possible : implicit type casting. 
+//		obj4.dis1(); // with help of obj4 we can call only those method which belong to super class
+//		//obj4.dis2();// or over rided methods. 
+//		B obj5 = (B) obj4;  // down level type casting 
+//		obj5.dis1();
+//		obj5.dis2();
+		A obj1 = new A();
+		Object obj2 = new A();		// sub 
+		
+		obj1.dis1();
+		//obj2.dis1();
+		A obj3 = (A)obj2;		// down level type casting 
+		obj3.dis1();
 	}
 
 }
