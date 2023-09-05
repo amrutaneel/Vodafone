66b97e3c490eccd814f54afb63b755ca34397222
Akash <akash300383@gmail.com>
2023-09-05 15:32:29 +0530

    done

--- workspace/jUnitTestApp/src/com/service/OperationTest.java
+++ workspace/jUnitTestApp/src/com/service/OperationTest.java
@@ -8,10 +8,29 @@ public class OperationTest {
 
 	@Test
 	public void testAdd() {
-		//fail("Not yet implemented");
 		Operation op = new Operation();
 		int result = op.add(100, 200);
 		assertEquals(300, result);
 	}
 
+	@Test
+	public void testSub() {
+		Operation op = new Operation();
+		int result = op.sub(1000, 500);
+		assertEquals(500, result);
+	}
+	
+	@Test
+	public void testMul() {
+		Operation op = new Operation();
+		int result = op.mul(10, 2);
+		assertEquals(20, result);
+	}
+	
+	@Test
+	public void testDiv() {
+		Operation op = new Operation();
+		int result = op.div(1000, 200);
+		assertEquals(5, result);
+	}
 }
